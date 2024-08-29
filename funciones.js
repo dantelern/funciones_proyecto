d3c60342bf97b0bf9af00e56d4277e2f669ccf86
dantelern <110490913+dantelern@users.noreply.github.com>
2024-08-29 11:23:56 -0300

    Create funciones.js

---
+++ funciones.js
@@ -0,0 +1,44 @@
+# Sopa de letras
+
+async function agarrarPalabras() {
+  try {
+    const respuesta = await axios.get('https://db-esi.vercel.app/api/puzzlewords');
+    puzzleWords = respuesta.data;
+    window.puzzleWords = puzzleWords;
+  } 
+  catch (error) {
+    console.error('Error al obtener las palabras:', error);
+    throw error;
+  }
+}
+
+agarrarPalabras();
+
+# Memotest
+
+const ImageList = () => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://db-esi.vercel.app')
+      .then(response => {
+        const imageLinks = response.data.map(item => item.img);
+        setImages(imageLinks);
+      })
+      .catch(error => {
+        console.error('Error al fetchear las imágenes:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {images.map((image, index) => (
+        <img key={index} src={image} alt={`Imagen ${index + 1}`} />
+      ))}
+    </div>
+  );
+};
+
+export default ImageList;
+
+# Users
