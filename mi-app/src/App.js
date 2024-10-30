feb3ddd7576c491a92d439aa8082a2e91dcd17b7
dantelern <110490913+dantelern@users.noreply.github.com>
2024-10-30 11:52:44 -0300

    agregué headers

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -3,6 +3,8 @@ import ImageList from './ImageList';
 import ListaPreguntas from './ListaPreguntas';
 import Login from './Login';
 import InfoTercero from './InfoTercero';
+import InfoPrimero from './InfoPrimero';
+import InfoSegundo from './InfoSegundo';
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
       <Login />
       <ImageList />
       <ListaPreguntas />
-      <InfoTercero />
+      <h1> Info Primero </h1> <InfoPrimero /> 
+      <h1> Info Segundo </h1> <InfoSegundo /> 
+      <h1> Info Tercero </h1> <InfoTercero /> 
     </div>
   );
 }
